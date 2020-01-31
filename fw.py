fd41ea7a65e005889e743417f36d1adbe88c56d6
jrelliott33 <jrelliott33@gmail.com>
2020-01-31 17:39:07 -0500

    corrections

--- fw.py
+++ fw.py
@@ -6,15 +6,13 @@ import shlex
 import subprocess
 import sys 
 
-IPT = 'sudo /usr/sbin/iptables'
 
-def check_sudo():
+def CheckSudo():
     ret = 0
     if os.geteuid() != 0:
         msg = "[sudo] password for %u:"
         ret = subprocess.check_call("sudo -v -p '%s'" % msg, shell=True)
     return ret
-
 
 def GetLocalIP():
     cmd = "ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}'"
@@ -26,48 +24,89 @@ def GetLocalIP():
         print(e)
         return 0
 
-def SetDefaultRules(localip):
+def SetDefaultRules():
+    localip = GetLocalIP()
     defaultrules = '''-F
                     -X
                     -t nat -F
                     -t nat -X
                     -t mangle -F
                     -t mangle -X
+                    -A INPUT -s {}/24 -i eth0 -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+                    -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
                     -P INPUT   DROP
                     -P FORWARD DROP
                     -P OUTPUT  DROP
-                    -A INPUT -s {}/24 -i eth0 -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
                     -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
                     -A INPUT -i lo -j ACCEPT
-                    -A OUTPUT -o eth0 -p tcp -m tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
                     -A OUTPUT -o eth0 -p tcp -m tcp --match multiport --dports 80,443 -m conntrack --ctstate NEW -j ACCEPT
-                    -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
                     -A OUTPUT -o lo -j ACCEPT
                     -A OUTPUT -p icmp -j ACCEPT
                     -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
                     -A INPUT -j DROP
                     -A FORWARD -j DROP
                     -A OUTPUT -j DROP'''.format(localip)
-    print (len(defaultrules.splitlines()))
+    
     for rule in defaultrules.splitlines():
-        cmd = IPT + " " + rule.strip()
-        try:
-            print(cmd)
-            cp = subprocess.run([cmd],shell=True,
-                                      check=True)
-            
-        except Exception as e: 
-            print("Error Setting Default Rules")
+        if not RunRule(rule.strip()):
+            print("Default Rules Not Set")
             return False
-       
+           
     print("Default Rules Set")
     return True
+   
+def PrintRules():
+    IPT = 'sudo /usr/sbin/iptables '
+    cmds = ['-nvL','-nvL -t nat']
+    for cmd in cmds:
+        try:
+            print(subprocess.check_output([IPT + cmd],shell=True).decode('utf-8'))
+            
+        except subprocess.CalledProcessError as e:            
+            print("Error Getting Rules: {}".format(e))
+            return False
 
-def AddRule(ip):
-    pass
+def RunRule(cmd,IPT = 'sudo /usr/sbin/iptables '):
+    
+    try:
+        cmd = IPT + cmd
+        cp = subprocess.check_call([cmd],shell=True)
+        return True
 
-def DeleteRule(ip):
-    pass
+    except subprocess.CalledProcessError as e:            
+        print("Error Setting Rule: {}".format(cmd))
+        return False
+
+def IptablesRules(ip,action="I",interface='eth0'):
+    # 192.168.1.1 192.168.1.1:80,443 :443
+
+    for item in ip:
+        ips = item.split(":",1)
+        if len(ips) > 2: 
+            print("ERROR: unknown string {}".format(ips))
+        elif len(ips) == 1:
+            #Handle only ips
+            outcmd = "-{} OUTPUT -o {} -p tcp -d {} -j ACCEPT".format(action,interface,ips[0])
+            incmd = "-{} INPUT -i {} -p tcp -s {} -j ACCEPT".format(action,interface,ips[0])
+            print(outcmd)
+            print(incmd)
+        elif len(ips) == 2:
+        #Handle multiple ports :PORT,PORT or IP:PORT,PORT
+        #TODO: Specify Direction???
+            if ips[0] != "" and ips[1] != "":
+                for port in ips[1].split(','):
+                    cmd = "-{} OUTPUT -o {} -p tcp -d {} --dport {} -j ACCEPT".format(action,interface,ips[0],port)
+                    print(cmd)
+                # Handle port only :<RHP>
+            elif ips[0] == "" and ips[1] != "":
+                for port in ips[1].split(','):
+                    cmd = "-{} OUTPUT -o {} -p tcp --dport {} -j ACCEPT".format(action,interface,port)
+                    print(cmd)
+            else: 
+                print("ERROR: Unkown Specification")                
+        else: 
+            print("ERROR: Unknown Value")
+            return False
 
 def FlushRules():
     rules = '''-P INPUT ACCEPT
@@ -81,55 +120,72 @@ def FlushRules():
                -t mangle -X'''
 
     for rule in rules.splitlines():
-        cmd = IPT + " " + rule.strip()
-        try: 
-            print(cmd)
-            cp = subprocess.run([cmd],shell=True,check=True)
-        except Exception as e:
-            print("Error Flushing Rules")
+        if not RunRule(rule.strip()):
+            print("Error Flushin Rules")
             return False
-        
+           
     print("Rules Flushed.")
     return True
 
-
-def SetForTor(ip,cmd):
-    pass
-
+def SetForTor(action='start'):
+    if action=='start':
+        cmd = ["sudo","kalitorify","-t"]
+    elif action == 'stop':
+        cmd = ["sudo","kalitorify","-c"]
+    try:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+ 
+        ## But do not wait till netstat finish, start displaying output immediately ##
+        while True:
+            out = p.stdout.read(1)
+            if out == '' and p.poll() != None:
+                break
+            if out != '':
+                sys.stdout.write(out.decode('utf-8'))
+                sys.stdout.flush()    
+        return True
+    except Exception as e:
+        print("ERROR setting up kalitorify, reseting default rules.")
+        print(e)
+        SetDefaultRules()
+        return False
 
 
 if __name__ == '__main__':
     #Check if sudo 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-A','--add', nargs='+')
-    parser.add_argument('-D','--delete', nargs='+')
+    parser.add_argument('-A','--add', nargs='+',help="Allow IP IP:PORT :PORT through firewall, ")
     parser.add_argument('-B','--block', nargs='+')
-    parser.add_argument('-R','--reset',action='store_true', help = 'reset default rules')
+    parser.add_argument('-D','--delete', nargs='+')
     parser.add_argument('-F','--flush',action='store_true',help='Flush and Accept All')
-    parser.add_argument('-T','--tor-start',action='store_true', help='Start Kalitorify')
-    parser.add_argument('-X','--tor-stop',action='store_true', help='Stop Kalitorify')
+    parser.add_argument('-P','--print',action='store_true',help='Print Firewall Rules')
+    parser.add_argument('-R','--reset',action='store_true', help = 'reset default rules')
+    parser.add_argument('-T','--starttor',action='store_true', help='Start Kalitorify')
+    parser.add_argument('-X','--stoptor',action='store_true', help='Stop Kalitorify')
     args = parser.parse_args()
-
-    if check_sudo() != 0:
+    if CheckSudo() != 0:
         print("Run with root")
         sys.exit()
     else:
-        try :
-            localip = GetLocalIP()
-        except:
-            sys.exit()
+        pass
 
-    print(localip)
     if args.add:
-        AddRule(localip)
+        IptablesRules(args.add)
     if args.delete:
-        DeleteRule(localip)
+        IptablesRules(args.delete,'D')
     if args.block:
-        BlockRule(localip)
+        #IptablesRules(args.delete,'block')
+        pass
+    if args.print:
+        PrintRules()
     if args.reset:
-        SetDefaultRules(localip)
+        SetDefaultRules()
     if args.flush:
         FlushRules()
+    if args.starttor:
+        SetForTor('start')
+    if args.stoptor:
+        SetForTor('stop')
     
 
 
